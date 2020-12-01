4eef6524cbd20ceae4821db5092dd9c5d0762ee3
Carson Graham <carson42g@gmail.com>
2020-12-01 16:28:48 -0500

    code formatting

--- src/main/kotlin/day1/Day1.kt
+++ src/main/kotlin/day1/Day1.kt
@@ -3,29 +3,26 @@ package day1
 import inputLines
 
 
-
-
-
 fun main1(): Int {
-   val numberSet = inputLines(1).filter { it.isNotBlank() }.map { it.toInt() }.toSet()
-   
-   for(i in numberSet) {
-      val need = 2020 - i
-      if(numberSet.contains(need)) return i * need;
-   }
-   return -1
+    val numberSet = inputLines(1).filter { it.isNotBlank() }.map { it.toInt() }.toSet()
+
+    for (i in numberSet) {
+        val need = 2020 - i
+        if (need in numberSet) return i * need
+    }
+    error("no solutions found")
 }
 
 fun main2(): Int {
-  val numberSet = inputLines(1).filter { it.isNotBlank() }.map { it.toInt() }.toSet()
+    val numberSet = inputLines(1).filter { it.isNotBlank() }.map { it.toInt() }.toSet()
 
-  for(i in numberSet) {
-     for(j in numberSet) {
-        val need = 2020 - i - j
-        if(numberSet.contains(need)) return i * j * need;
-     }
-  }
-  return -1
+    for (i in numberSet) {
+        for (j in numberSet) {
+            val need = 2020 - i - j
+            if (need in numberSet) return i * j * need
+        }
+    }
+    error("no solutions found")
 }
 
 fun main() = println(main2())
